a1c0862e2a843aaaf7c41e3c4a53a43a013e5b12
Kamal Kumar <140941027+kamal1122pk@users.noreply.github.com>
2026-03-31 12:59:42 +0500

    Update wsgi.py ok

--- backend/backend_core/wsgi.py
+++ backend/backend_core/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from auto_migrate import run_migrations  # import 
+
+# Run migrations once on startup
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_core.settings')
 
 application = get_wsgi_application()
+
+run_migrations()
