84a7e8c8a5bf372e8d04b386abab424b540e216d
Kamal Kumar <140941027+kamal1122pk@users.noreply.github.com>
2026-03-31 13:21:00 +0500

    Update wsgi.py oka

--- backend/backend_core/wsgi.py
+++ backend/backend_core/wsgi.py
@@ -1,10 +1,26 @@
+"""
+WSGI config for backend_core project.
+
+It exposes the WSGI callable as a module-level variable named `application`.
+
+For more information:
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
 import os
 from django.core.wsgi import get_wsgi_application
-from django.core.management import call_command
 
+# Set the settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_core.settings')
 
-# Run migrations (careful: every cold start!)
-call_command("migrate", interactive=False)
-
+# Get the WSGI application
 application = get_wsgi_application()
+
+# NOTE:
+# Do NOT run migrations here.
+# On Vercel (serverless), this file can be imported multiple times, 
+# and running migrations here will cause AppRegistryNotReady errors 
+# and race conditions on the database.
+
+# Migrations should be run once during deployment using a separate command:
+#   python manage.py migrate --noinput
